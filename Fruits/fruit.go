77bf6b4592e53d437c19f6ce74562a210da29471
laksd <laksd@nvidia.com>
2025-09-02 23:53:31 -0700

    New fruit for brain health

--- Fruits/fruit.go
+++ Fruits/fruit.go
@@ -3,3 +3,4 @@ package Fruits
 var ImmunityFruit string = "Lemon"
 var BloodPurifierFruit string = "Pomegranate"
 var HeartHealthyFruit string = "Jackfruit"
+var BrainHealthyFruit string = "Avocado"
